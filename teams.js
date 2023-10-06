1275563de105e8e8fb46a6ec4e0b8c3ec070115a
niyasali96 <136828928+niyasali96@users.noreply.github.com>
2023-10-07 03:11:15 +0400

    teams.js

--- teams.js
+++ teams.js
@@ -8,11 +8,24 @@
     }
 
     // Insert text into the eight <p> elements using the function.
+  // Match 1
+  setTextContent("match1", "Match Name");
     setTextContent("team1", "Text for team 1");
     setTextContent("team2", "Text for team 2");
+    setTextContent("time1", "10:30PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 2
+  setTextContent("match2", "Match Name");
     setTextContent("team3", "Text for team 3");
     setTextContent("team4", "Text for team 4");
+    setTextContent("time2", "10:30PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 3
+  setTextContent("match3", "Match Name");
     setTextContent("team5", "Text for team 5");
     setTextContent("team6", "Text for team 6");
+    setTextContent("time3", "10:30PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 4
+  setTextContent("match4", "Match Name");
     setTextContent("team7", "Text for team 7");
     setTextContent("team8", "Text for team 8");
+    setTextContent("time4", "10:30PM IST | 10:00PM GST | 10:00PM AST");
+                   
