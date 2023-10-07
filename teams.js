055fd4a9d23a0e7956df38959cef65a7fe392a23
niyasali96 <136828928+niyasali96@users.noreply.github.com>
2023-10-07 15:00:41 +0400

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,4 +1,4 @@
-    // Function to safely set text content of an element if it exists.
+// Function to safely set text content of an element if it exists.
     function setTextContent(id, text) {
       var element = document.getElementById(id);
       if (element) {
@@ -15,27 +15,14 @@
     }
 
     // Insert text and set team logos using the functions.
-        // Insert text into the eight <p> elements using the function.
-  // Match 1
-  setTextContent("match1", "Match Name");
-    setTextContent("team1", "Text for team1");
+    setTextContent("team1", "Text for team 1");
     setTextContent("team2", "Text for team 2");
-    setTextContent("time1", "10:35PM IST | 10:00PM GST | 10:00PM AST");
-  // Match 2
-  setTextContent("match2", "Match Name");
     setTextContent("team3", "Text for team 3");
     setTextContent("team4", "Text for team 4");
-    setTextContent("time2", "10:35PM IST | 10:00PM GST | 10:00PM AST");
-  // Match 3
-  setTextContent("match3", "Match Name");
     setTextContent("team5", "Text for team 5");
     setTextContent("team6", "Text for team 6");
-    setTextContent("time3", "10:35PM IST | 10:00PM GST | 10:00PM AST");
-  // Match 4
-  setTextContent("match4", "Match Name");
     setTextContent("team7", "Text for team 7");
     setTextContent("team8", "Text for team 8");
-    setTextContent("time4", "10:35PM IST | 10:00PM GST | 10:00PM AST");
 
     setTeamLogoSrc("team1-logo", "https://logowik.com/content/uploads/images/brazil-national-football-team1330.logowik.com.webp");
     setTeamLogoSrc("team2-logo", "team2-logo.jpg");
@@ -45,4 +32,3 @@
     setTeamLogoSrc("team6-logo", "team6-logo.jpg");
     setTeamLogoSrc("team7-logo", "team7-logo.jpg");
     setTeamLogoSrc("team8-logo", "team8-logo.jpg");
-    
