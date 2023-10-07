a0a68a32de8ade10e136253db047e19db395c4c5
niyasali96 <136828928+niyasali96@users.noreply.github.com>
2023-10-07 23:42:22 +0400

    Create slide.js

---
+++ slide.js
@@ -0,0 +1,81 @@
+    // Function to safely set text content of an element if it exists.
+    function setTextContent(id, text) {
+      var element = document.getElementById(id);
+      if (element) {
+        element.textContent = text;
+      }
+    }
+
+    // Function to set the src attribute of an img element if it exists.
+    function setTeamLogoSrc(id, src) {
+      var imgElement = document.getElementById(id);
+      if (imgElement) {
+        imgElement.src = src;
+      }
+    }
+
+    // Insert text and set team logos using the functions.
+        // Insert text into the eight <p> elements using the function.
+  // Match 1
+  setTextContent("slidematch1", "Match Name");
+    setTextContent("slideteam1", "Text for team1");
+    setTextContent("slideteam2", "Text for team 2");
+    setTextContent("slidetime1", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 2
+  setTextContent("slidematch2", "Match Name");
+    setTextContent("slideteam3", "Text for team 3");
+    setTextContent("slideteam4", "Text for team 4");
+    setTextContent("slidetime2", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 3
+  setTextContent("slidematch3", "Match Name");
+    setTextContent("slideteam5", "Text for team 5");
+    setTextContent("slideteam6", "Text for team 6");
+    setTextContent("slidetime3", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 4
+  setTextContent("slidematch4", "Match Name");
+    setTextContent("slideteam7", "Text for team 7");
+    setTextContent("slideteam8", "Text for team 8");
+    setTextContent("slidetime4", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 5
+  setTextContent("slidematch5", "Match Name");
+    setTextContent("slideteam9", "Text for team 9");
+    setTextContent("slideteam10", "Text for team 10");
+    setTextContent("slidetime5", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 6
+  setTextContent("slidematch6", "Match Name");
+    setTextContent("slideteam11", "Text for team 11");
+    setTextContent("slideteam12", "Text for team 12");
+    setTextContent("slidetime6", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 7
+  setTextContent("slidematch7", "Match Name");
+    setTextContent("slideteam13", "Text for team 13");
+    setTextContent("slideteam14", "Text for team 14");
+    setTextContent("slidetime7", "10:35PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 8
+  setTextContent("slidematch8", "Match Name");
+    setTextContent("slideteam15", "Text for team 15");
+    setTextContent("slideteam16", "Text for team 16");
+    setTextContent("slidetime8", "10:40PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 9
+  setTextContent("slidematch9", "Match Name");
+    setTextContent("slideteam17", "Text for team 15");
+    setTextContent("slideteam18", "Text for team 16");
+    setTextContent("slidetime9", "10:40PM IST | 10:00PM GST | 10:00PM AST");
+  // Match 10
+  setTextContent("slidematch10", "Match Name");
+    setTextContent("slideteam19", "Text for team 15");
+    setTextContent("slideteam20", "Text for team 16");
+    setTextContent("slidetime10", "10:40PM IST | 10:00PM GST | 10:00PM AST");
+
+
+    setTeamLogoSrc("slideteam1-logo", "https://banner2.cleanpng.com/20180718/a/kisspng-brazil-national-football-team-2014-fifa-world-cup-camisa-brasil-5b4f1a28b2c693.9926342415319106967323.jpg");
+    setTeamLogoSrc("slideteam2-logo", "https://www.uaefa.ae/api/public/img/logo/e8cb1894e09541c690a69ad547fef0c5.png");
+    setTeamLogoSrc("slideteam3-logo", "https://static.toiimg.com/thumb/msid-77700605,width-400,resizemode-4/77700605.jpg");
+    setTeamLogoSrc("slideteam4-logo", "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Chennai_Super_Kings_Logo.svg/1200px-Chennai_Super_Kings_Logo.svg.png");
+    setTeamLogoSrc("slideteam5-logo", "slideteam5-logo.jpg");
+    setTeamLogoSrc("slideteam6-logo", "slideteam6-logo.jpg");
+    setTeamLogoSrc("slideteam7-logo", "slideteam7-logo.jpg");
+    setTeamLogoSrc("slideteam8-logo", "slideteam8-logo.jpg");
+    setTeamLogoSrc("slideteam9-logo", "slideteam9-logo.jpg");
+    setTeamLogoSrc("slideteam10-logo", "slideteam10-logo.jpg");
+    
