9a5ba7bdab03e62bb54cb72026062220d78599a6
niyasali96 <136828928+niyasali96@users.noreply.github.com>
2023-10-07 08:22:01 +0400

    teams.js

--- teams.js
+++ teams.js
@@ -1,4 +1,3 @@
-
     // Function to safely set text content of an element if it exists.
     function setTextContent(id, text) {
       var element = document.getElementById(id);
@@ -7,7 +6,16 @@
       }
     }
 
-    // Insert text into the eight <p> elements using the function.
+    // Function to set the src attribute of an <img> element if it exists.
+    function setTeamLogoSrc(id, src) {
+      var imgElement = document.getElementById(id);
+      if (imgElement) {
+        imgElement.src = src;
+      }
+    }
+
+    // Insert text and set team logos using the functions.
+        // Insert text into the eight <p> elements using the function.
   // Match 1
   setTextContent("match1", "Match Name");
     setTextContent("team1", "Text for team1");
@@ -27,5 +35,13 @@
   setTextContent("match4", "Match Name");
     setTextContent("team7", "Text for team 7");
     setTextContent("team8", "Text for team 8");
-    setTextContent("time4", "10:30PM IST | 10:00PM GST | 10:00PM AST");
-                   
+
+    setTeamLogoSrc("team1-logo", "https://logowik.com/content/uploads/images/brazil-national-football-team1330.logowik.com.webp");
+    setTeamLogoSrc("team2-logo", "team2-logo.jpg");
+    setTeamLogoSrc("team3-logo", "team3-logo.jpg");
+    setTeamLogoSrc("team4-logo", "team4-logo.jpg");
+    setTeamLogoSrc("team5-logo", "team5-logo.jpg");
+    setTeamLogoSrc("team6-logo", "team6-logo.jpg");
+    setTeamLogoSrc("team7-logo", "team7-logo.jpg");
+    setTeamLogoSrc("team8-logo", "team8-logo.jpg");
+    
