48cc5b949c5982d4ee116a03165b121d77cd0b82
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2024-04-07 10:03:46 +0400

    Delete basic-script.js

--- basic-script.js
+++
@@ -1,9 +0,0 @@
-if (Hls.isSupported()) {
-    var video = document.getElementById('live-stream');
-    var hls = new Hls();
-    hls.loadSource('https://iplstream.noon.com/hls/live/2098770/NOON/7607/89752/master.m3u8');
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function() {
-     video.play();
-    });
-   }
